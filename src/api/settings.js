e2a0180270f4ee1d9d74d3b94d45b99c67fb3aeb
lizheyong <740967133@qq.com>
2025-06-06 04:52:16 +0800

    test actions

---
+++ src/api/settings.js
@@ -0,0 +1,3 @@
+export const settingsApi = {
+  // API 方法
+};
---
+++ src/pages/ProfileInfo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="profile-info">
+    <!-- 页面内容 -->
+  </div>
+</template>
+
+<script setup>
+// 组件逻辑
+</script>
+
+<style scoped>
+/* 样式 */
+</style>
